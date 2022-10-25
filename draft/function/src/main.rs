fde3f0cc52274b697eb7a276921499a1ef934f78
SAV <ctf149@gmail.com>
2022-10-25 17:50:41 +0300

    Progress commit

--- draft/function/src/main.rs
+++ draft/function/src/main.rs
@@ -1,9 +1,36 @@
 fn main() {
     println!("First Function!");
     another_function();
+    get_numb_square(11);
+    get_numb_summ(17, 5);
+    block_exmpl();
+    println!("Func Five = {}", five())
 }
 
 fn another_function() {
     println!("Second Function!");
 }
 
+fn get_numb_square(x: i32) {
+    let result = x*x;
+    println!("Square = {}", result);
+}
+
+fn get_numb_summ(x: i32, y: i32){
+    let result = x + y;
+    println!("Summ = {}", result);
+}
+
+fn block_exmpl() {
+    let x = 5;
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("Value from block is {}", y);
+}
+
+fn five () -> i32 {
+    22
+}
