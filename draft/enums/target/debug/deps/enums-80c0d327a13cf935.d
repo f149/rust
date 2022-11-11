cdcdf36f1a461ada764af62ed494bc510bea51db
SAV <ctf149@gmail.com>
2022-11-11 10:39:27 +0200

    Start ENUM

---
+++ draft/enums/target/debug/deps/enums-80c0d327a13cf935.d
@@ -0,0 +1,7 @@
+/Users/av/Work/rust/draft/enums/target/debug/deps/enums-80c0d327a13cf935.rmeta: src/main.rs
+
+/Users/av/Work/rust/draft/enums/target/debug/deps/enums-80c0d327a13cf935.d: src/main.rs
+
+src/main.rs:
+
+# env-dep:CLIPPY_ARGS=
---
+++ draft/enums/target/debug/deps/enums-b7a367b3aaa7c227.d
@@ -0,0 +1,7 @@
+/Users/av/Work/rust/draft/enums/target/debug/deps/enums-b7a367b3aaa7c227.rmeta: src/main.rs
+
+/Users/av/Work/rust/draft/enums/target/debug/deps/enums-b7a367b3aaa7c227.d: src/main.rs
+
+src/main.rs:
+
+# env-dep:CLIPPY_ARGS=
