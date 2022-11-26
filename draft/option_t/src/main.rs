b5957f2527964871854efe0732d1e89f5f779fca
SAV <ctf149@gmail.com>
2022-11-26 13:12:03 +0200

    Update repo

--- draft/option_t/src/main.rs
+++ draft/option_t/src/main.rs
@@ -1,2 +1,15 @@
 fn main() {
+
+    let some_number = Some(5);
+    let some_string = Some("String");
+    let absent_numb: Option<i32> = None;
+
 }
+
+/* Определение Option<T> в стандартной библиотеке
+enum OptionSmpl<T> {
+    Some(T),
+    None,
+}
+*/
+
